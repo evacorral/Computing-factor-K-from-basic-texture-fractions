5381a4d4bf9c7c4da5381b93e3c6ece0d18f9e98
ejerezl <44298472+ejerezl@users.noreply.github.com>
2022-01-21 13:20:43 +0100

    Add files via upload

--- computingK.R
+++ computingK.R
@@ -17,17 +17,7 @@ computingK <- function(){
   col1 = names(input_data[1])
   col2 = names(input_data[2])
   col3 = names(input_data[3])
-  
-  for (i in 1:nrow(input_data)) {
-    ele1 = as.numeric(input_data[i,1])
-    ele2 = as.numeric(input_data[i,2])
-    ele3 = as.numeric(input_data[i,3])
-    sum = (ele1 + ele2 + ele3)
-    
-    if ( floor(sum) != 100) {
-      cat("---- WARNING! Data with values: \n", col1, ele1, "\n", col2, ele2, "\n", col3, ele3, "\nhas wrong quantities, it will be left blank\n")  
-    }
-  }
+
   
   # Used for giving the output data in the same order as the input
   input_data$id <- 1:nrow(input_data)
@@ -46,6 +36,7 @@ computingK <- function(){
     sand = output_data[i,1]
     silt = output_data[i,2]
     clay = output_data[i,3]
+    sum = sand + silt + clay
     VFS = output_data[i,4]
     a = output_data[i, 5]
     soil = output_data[i, 6]
@@ -53,22 +44,24 @@ computingK <- function(){
     VFS2 = output_data[i,8]
     VFS1 = output_data[i,9]
     VFS3 = output_data[i,10]
+    
     check[i] <- NA
+    vectorq1[i] <- NA
+    vectorq2[i] <- NA
+    vectorq3[i] <- NA
     #First we check if this data is correct or not. If it is not correct, then the entries of VFS1, VFS2, VFS3 will be empty
-    if(decimalplaces(sand) || decimalplaces(silt) || decimalplaces(clay) || decimalplaces(VFS)) {
-      cat("WARNING! All SandUSDA, SiltUSDA, ClayUSDA and VFS_USDA must have at most 1 decimal. That data will be left with a blank space\n")
+    
+    if ( floor(sum) != 100) {
+      cat("---- WARNING! Data with values: \n", names(input_data[1]),":", sand, "\n", names(input_data[2]), ": ", silt ,"\n", names(input_data[3]),":", clay, "\n", "has wrong quantities(sand + silt + clay must be equal to 100), it will be left blank\n")
+      
+    } else if(decimalplaces(sand) || decimalplaces(silt) || decimalplaces(clay) ) {
+      cat("---- WARNING! All SandUSDA, SiltUSDA and ClayUSDA must have at most 1 decimal. That data will be left with a blank space\n")
       cat("The data of that wrong entry is: \n")
       cat ( names(input_data[1]),":", sand, "\n", names(input_data[2]), ": ", silt ,"\n", names(input_data[3]),":", clay, "\n", names(input_data[4]),":", VFS ,"\n", names(input_data[5]),": ", a, "\n", names(input_data[6]), ": ", soil, "\n", names(input_data[7]), ": ", permeability,"\n")
-      vectorq1[i] <- NA
-      vectorq2[i] <- NA
-      vectorq3[i] <- NA
     } else if (is.na(VFS1) || is.na(VFS2) || is.na(VFS3)) {
       cat("Estimate of VFS not found for the following entry: \n")
       cat ( names(input_data[1]),":", sand, "\n", names(input_data[2]), ": ", silt ,"\n", names(input_data[3]),":", clay, "\n", names(input_data[4]),":", VFS ,"\n", names(input_data[5]),": ", a, "\n", names(input_data[6]), ": ", soil, "\n", names(input_data[7]), ": ", permeability,"\n")
       cat("That entry will be left with a blank space.\n")
-      vectorq1[i] <- NA
-      vectorq2[i] <- NA
-      vectorq3[i] <- NA
     } else if (is.element(soil, c(1,2,3,4)) && is.element(permeability, c(1,2,3,4,5,6))) {
       # We check if the values of the soil structure and permeability are correct
       
@@ -76,7 +69,7 @@ computingK <- function(){
         # We also check for the organic matter
         
         if ((clay < 12 && silt > 80) || (clay > 40 && silt < 40) || (clay > 20 && clay <= 40 && sand > 45)) {
-          cat("WARNING! Texture out of range of Factor K definition for the following entry: \n")
+          cat("---- WARNING! Texture out of range of Factor K definition for the following entry: \n")
           cat ( names(input_data[1]),":", sand, "\n", names(input_data[2]), ": ", silt ,"\n", names(input_data[3]),":", clay, "\n", names(input_data[4]),":", VFS ,"\n", names(input_data[5]),": ", a, "\n", names(input_data[6]), ": ", soil, "\n", names(input_data[7]), ": ", permeability,"\n")
           cat("An extrapolation is being done. \n")
           check[i] <- 'EXTRAPOLATION-OUT OF RANGE'
@@ -90,54 +83,54 @@ computingK <- function(){
           if(is.na(vectorq1[i]) || is.na(vectorq2[i]) || is.na(vectorq3[i])) {
             # If the output is NA, then the reason is because of the M parameter
             
-            cat("WARNING! Result of parameter M is bigger tan 8000. You can call help() for generic information about the usage of this program. That data will be left with a blank space\n")
-            cat("The data of that wrong entry is: \n", names(input_data[1]),":", sand, "\n", names(input_data[2]), ": ", silt ,"\n", names(input_data[3]),":", clay, "\n", names(input_data[4]),": ", a, "\n", names(input_data[4]), ": ", soil, "\n", names(input_data[5]), ": ", permeability,"\n")
+            cat("---- WARNING! Result of parameter M is bigger tan 8000. You can call help_K() for generic information about the usage of this program. That data will be left with a blank space\n")
+            cat("The data of that wrong entry is: \n")
+            cat ( names(input_data[1]),":", sand, "\n", names(input_data[2]), ": ", silt ,"\n", names(input_data[3]),":", clay, "\n", names(input_data[4]),":", VFS ,"\n", names(input_data[5]),": ", a, "\n", names(input_data[6]), ": ", soil, "\n", names(input_data[7]), ": ", permeability,"\n")
           }
         } else {
           # Checking if VFS_USDA > SandUSDA
           if (VFS > sand) {
-            cat('WARNING! VFS_USDA is greater than SandUSDA. Factor K is not being computed.')
+            cat('---- WARNING! VFS_USDA is greater than SandUSDA. Factor K is not being computed.')
+            cat("The data of that wrong entry is: \n")
+            cat ( names(input_data[1]),":", sand, "\n", names(input_data[2]), ": ", silt ,"\n", names(input_data[3]),":", clay, "\n", names(input_data[4]),":", VFS ,"\n", names(input_data[5]),": ", a, "\n", names(input_data[6]), ": ", soil, "\n", names(input_data[7]), ": ", permeability,"\n")
             vectorq2[i] <- NA
           } else {
             vectorq2[i] <- K_equations(VFS, silt, sand, a, soil, permeability)
             if(is.na(vectorq2[i])) {
               # If the output is NA, then the reason is because of the M parameter
               
-              cat("WARNING! Result of parameter M is bigger tan 8000. You can call help() for generic information about the usage of this program. That data will be left with a blank space\n")
-              cat("The data of that wrong entry is: \n", names(input_data[1]),":", sand, "\n", names(input_data[2]), ": ", silt ,"\n", names(input_data[3]),":", clay, "\n", names(input_data[4]),": ", a, "\n", names(input_data[4]), ": ", soil, "\n", names(input_data[5]), ": ", permeability,"\n")
+              cat("---- WARNING! Result of parameter M is bigger tan 8000. You can call help_K() for generic information about the usage of this program. That data will be left with a blank space\n")
+              cat("The data of that wrong entry is: \n")
+              cat ( names(input_data[1]),":", sand, "\n", names(input_data[2]), ": ", silt ,"\n", names(input_data[3]),":", clay, "\n", names(input_data[4]),":", VFS ,"\n", names(input_data[5]),": ", a, "\n", names(input_data[6]), ": ", soil, "\n", names(input_data[7]), ": ", permeability,"\n")
             }
           }
           output_data[i,8] <- NA
           output_data[i,9] <- NA
           output_data[i,10] <- NA
-          vectorq1[i] <- NA
-          vectorq3[i] <- NA
         }
         
       } else {
-        cat("--------WARNING! There is something wrong with the value of the organic matter.")
+        cat("---- WARNING! There is something wrong with the value of the organic matter.")
         cat("RECALL THAT: \n")
-        cat("* O.M. : organic matter. Must be a value from 0.0 to 4.0 (included) with one decimal.")
-        cat("For more information call help(\"input data\")\n")
-        cat("That entry will be left blank when doing the computations")
-        vectorq1[i] <- NA
-        vectorq2[i] <- NA
-        vectorq3[i] <- NA
-      }
+        cat("* O.M. : organic matter. Must be a value from 0.0 to 4.0 (included) with one decimal. \n")
+        cat("For more information call help_K(\"input data\")\n")
+        cat("That entry will be left blank when doing the computations\n")
+        cat("The data of that wrong entry is: \n")
+        cat ( names(input_data[1]),":", sand, "\n", names(input_data[2]), ": ", silt ,"\n", names(input_data[3]),":", clay, "\n", names(input_data[4]),":", VFS ,"\n", names(input_data[5]),": ", a, "\n", names(input_data[6]), ": ", soil, "\n", names(input_data[7]), ": ", permeability,"\n")
+        
+        }
       
     } else {
-      cat("--------WARNING! There is something wrong with atribute permeability or soil structure. The data of that wrong entry is: \n", names(input_data[1]),":", sand, "\n", names(input_data[2]), ": ", silt ,"\n", names(input_data[3]),":", clay, "\n", names(input_data[4]),": ", a, "\n", names(input_data[5]), ": ", soil, "\n", names(input_data[6]), ": ", permeability,"\n")
-     
+      cat("---- WARNING! There is something wrong with atribute permeability or soil structure. The data of that wrong entry is: \n", names(input_data[1]),":", sand, "\n", names(input_data[2]), ": ", silt ,"\n", names(input_data[3]),":", clay, "\n", names(input_data[4]),": ", a, "\n", names(input_data[5]), ": ", soil, "\n", names(input_data[6]), ": ", permeability,"\n")
+      cat("The data of that wrong entry is: \n")
+      cat ( names(input_data[1]),":", sand, "\n", names(input_data[2]), ": ", silt ,"\n", names(input_data[3]),":", clay, "\n", names(input_data[4]),":", VFS ,"\n", names(input_data[5]),": ", a, "\n", names(input_data[6]), ": ", soil, "\n", names(input_data[7]), ": ", permeability,"\n")
       cat("RECALL THAT: \n")
       cat("* Soil structure: must be a number from 1 to 4 with the following meanings: \n")
       cat ("       1 = very fine granular\n       2 = fine granular\n       3 = moderate or coorse granular\n       4 = blocky, platy, or massive\n")
       cat("* Permeability: must be a number from 1 to 6 with the following meanings: \n")
       cat ("       1 = rapid\n       2 = moderate to rapid\n       3 = moderate\n       4 = slow to moderate\n       5 = slow\n       6 = very slow\n")
-      cat("For more information call help(\"input data\")\n")
+      cat("For more information call help_K(\"input data\")\n")
       cat("That entry will be left blank when doing the computations")
-      vectorq1[i] <- NA
-      vectorq2[i] <- NA
-      vectorq3[i] <- NA
     }
     
   }
